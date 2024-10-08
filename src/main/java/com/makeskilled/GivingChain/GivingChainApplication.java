3893edccc6ab4757841b79f22241d85db2fc95da
MS <72651078+maddydevgits@users.noreply.github.com>
2024-10-08 12:24:13 +0530

    setting up java springboot project

---
+++ src/main/java/com/makeskilled/GivingChain/GivingChainApplication.java
@@ -0,0 +1,13 @@
+package com.makeskilled.GivingChain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GivingChainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GivingChainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makeskilled/GivingChain/GivingChainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makeskilled.GivingChain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GivingChainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
